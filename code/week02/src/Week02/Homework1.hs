308cb591d424c3367f39d06aad7433b16bef055f
calin <calinoiu.alexandru@agilefreaks.com>
2021-10-06 18:01:39 +0300

    Fix homework1

--- code/week02/src/Week02/Homework1.hs
+++ code/week02/src/Week02/Homework1.hs
@@ -8,47 +8,66 @@
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
-
 module Week02.Homework1 where
 
-import           Control.Monad        hiding (fmap)
-import           Data.Map             as Map
-import           Data.Text            (Text)
-import           Data.Void            (Void)
-import           Plutus.Contract
-import qualified PlutusTx
-import           PlutusTx.Prelude     hiding (Semigroup(..), unless)
-import           Ledger               hiding (singleton)
-import           Ledger.Constraints   as Constraints
-import qualified Ledger.Typed.Scripts as Scripts
-import           Ledger.Ada           as Ada
-import           Playground.Contract  (printJson, printSchemas, ensureKnownCurrencies, stage)
-import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types     (KnownCurrency (..))
-import           Prelude              (IO, Semigroup (..), String, undefined)
-import           Text.Printf          (printf)
+import           Control.Monad               (void, (>>), (>>=))
+import           Data.Text                   (Text)
+import           GHC.Base                    (Monoid (mconcat), String, ($),
+                                              (<>))
+import           GHC.Num                     (Integer)
+import           Ledger                      (Address, Redeemer (Redeemer),
+                                              ScriptContext, Validator,
+                                              scriptAddress, txId)
+import           Ledger.Ada                  as Ada (lovelaceValueOf)
+
+import           Data.Functor                ((<$>))
+import qualified Data.Map                    as Map
+import           Data.Tuple                  (fst)
+import           Data.Void                   (Void)
+import qualified Ledger.Constraints          as Constraints
+import           Playground.TH               (mkKnownCurrencies,
+                                              mkSchemaDefinitions)
+import           Playground.Types            (KnownCurrency (..))
+import           Plutus.Contract             (AsContractError, Contract,
+                                              Endpoint, awaitTxConfirmed,
+                                              endpoint, logInfo, select,
+                                              submitTxConstraints,
+                                              submitTxConstraintsWith,
+                                              type (.\/), utxoAt)
+import           Plutus.Contract.Constraints (TxConstraints, mustPayToTheScript,
+                                              mustSpendScriptOutput)
+import           PlutusTx                    (IsData (toData), compile)
+import           PlutusTx.Prelude            (Bool, traceIfFalse, (&&))
+import           Text.Printf                 (printf)
+
+-- Needed for playground
+import qualified Ledger.Typed.Scripts        as Scripts
+import           Playground.Contract         (ensureKnownCurrencies, printJson,
+                                              printSchemas, stage)
+import           Prelude                     (IO)
 
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are equal!
 mkValidator :: () -> (Bool, Bool) -> ScriptContext -> Bool
-mkValidator _ _ _ = True -- FIX ME!
+mkValidator _ (b, b') _ = traceIfFalse "There are not both TRUE" b && b'
 
 data Typed
 instance Scripts.ValidatorTypes Typed where
--- Implement the instance!
+    type instance DatumType Typed = ()
+    type instance RedeemerType Typed = (Bool, Bool)
 
 typedValidator :: Scripts.TypedValidator Typed
-typedValidator = undefined -- FIX ME!
+typedValidator = Scripts.mkTypedValidator @Typed
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+    where
+        wrap = Scripts.wrapValidator @() @(Bool, Bool)
 
 validator :: Validator
-validator = undefined -- FIX ME!
-
-valHash :: Ledger.ValidatorHash
-valHash = undefined -- FIX ME!
+validator = Scripts.validatorScript typedValidator
 
 scrAddress :: Ledger.Address
-scrAddress = undefined -- FIX ME!
+scrAddress = scriptAddress validator
 
 type GiftSchema =
             Endpoint "give" Integer
