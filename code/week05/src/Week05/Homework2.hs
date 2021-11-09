008652e090b940a3185310e1207c67e4519c10ce
calin <calinoiu.alexandru@agilefreaks.com>
2021-11-09 23:18:07 +0200

    Add solution 2

--- code/week05/src/Week05/Homework2.hs
+++ code/week05/src/Week05/Homework2.hs
@@ -12,50 +12,93 @@
 
 module Week05.Homework2 where
 
-import           Control.Monad          hiding (fmap)
+import           Control.Monad          (Monad ((>>)), void)
 import qualified Data.Map               as Map
 import           Data.Text              (Text)
 import           Data.Void              (Void)
-import           Plutus.Contract        as Contract
-import           Plutus.Trace.Emulator  as Emulator
-import qualified PlutusTx
-import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
-import           Ledger                 hiding (mint, singleton)
-import           Ledger.Constraints     as Constraints
+import           Ledger                 (CurrencySymbol,
+                                         ScriptContext (scriptContextTxInfo),
+                                         TokenName, TxInInfo (txInInfoOutRef),
+                                         TxInfo (txInfoForge, txInfoInputs),
+                                         TxOutRef, mkMintingPolicyScript,
+                                         pubKeyAddress, scriptCurrencySymbol,
+                                         txId)
+import           Ledger.Constraints     as Constraints (mintingPolicy,
+                                                        mustMintValue,
+                                                        mustSpendPubKeyOutput,
+                                                        unspentOutputs)
 import qualified Ledger.Typed.Scripts   as Scripts
-import           Ledger.Value           as Value
-import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
-import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types       (KnownCurrency (..))
-import           Prelude                (IO, Semigroup (..), Show (..), String, undefined)
+import           Ledger.Value           as Value (flattenValue, singleton)
+import           Plutus.Contract        as Contract (Contract, Endpoint,
+                                                     awaitTxConfirmed, endpoint,
+                                                     logError, logInfo,
+                                                     ownPubKey,
+                                                     submitTxConstraintsWith,
+                                                     utxoAt)
+import           Plutus.Trace.Emulator  as Emulator (activateContractWallet,
+                                                     callEndpoint,
+                                                     runEmulatorTraceIO,
+                                                     waitNSlots)
+import           Plutus.V1.Ledger.Api   (TokenName (TokenName))
+import qualified PlutusTx
+import           PlutusTx.Prelude       (Bool (False), Eq ((==)), any,
+                                         emptyByteString, traceIfFalse, ($),
+                                         (&&), (.))
+import           Prelude                (IO, Semigroup (..), Show (..), String)
 import           Text.Printf            (printf)
-import           Wallet.Emulator.Wallet
+import           Wallet.Emulator.Wallet (Wallet (Wallet))
+
+{-# INLINABLE tn #-}
+tn :: TokenName
+tn = TokenName emptyByteString
 
 {-# INLINABLE mkPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkPolicy oref () ctx = True -- FIX ME!
+mkPolicy oref () ctx = traceIfFalse "must consum UTxO" consumesOutput &&
+                       traceIfFalse "must have correct value" isValueCorrect
+    where
+      info :: TxInfo
+      info = scriptContextTxInfo ctx
+
+      consumesOutput :: Bool
+      consumesOutput = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info
+
+      isValueCorrect :: Bool
+      isValueCorrect = case flattenValue (txInfoForge info) of
+        [(_, tn', amt)] -> amt == 1 && tn' == tn
+        _               -> False
 
 policy :: TxOutRef -> Scripts.MintingPolicy
-policy oref = undefined -- IMPLEMENT ME!
+policy oref = mkMintingPolicyScript $
+    $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy ||])
+    `PlutusTx.applyCode`
+    PlutusTx.liftCode oref
 
 curSymbol :: TxOutRef -> CurrencySymbol
-curSymbol = undefined -- IMPLEMENT ME!
+curSymbol = scriptCurrencySymbol . policy
 
 type NFTSchema = Endpoint "mint" ()
 
 mint :: Contract w NFTSchema Text ()
-mint = undefined -- IMPLEMENT ME!
+mint = do
+  pk <- Contract.ownPubKey
+  utxos <- utxoAt (pubKeyAddress pk)
+  case Map.keys utxos of
+    [] -> Contract.logError @String "No utxos found"
+    utxo:_ -> do
+      let val = Value.singleton (curSymbol utxo) "" 1
+          lookups = Constraints.mintingPolicy (policy utxo) <> Constraints.unspentOutputs utxos
+          tx = Constraints.mustMintValue val <> Constraints.mustSpendPubKeyOutput utxo
+      ledgerTx <- submitTxConstraintsWith @Void lookups tx
+      void $ awaitTxConfirmed $ txId ledgerTx
+      Contract.logInfo @String $ printf "Minted %s" (show val)
 
 endpoints :: Contract () NFTSchema Text ()
 endpoints = mint' >> endpoints
   where
     mint' = endpoint @"mint" >> mint
-
-mkSchemaDefinitions ''NFTSchema
-
-mkKnownCurrencies []
 
 test :: IO ()
 test = runEmulatorTraceIO $ do
